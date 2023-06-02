1e23c173bc4964c88c0f1ff6b9fa5ace3107b773
maha <pratyushsipu81@hmail.com>
2023-06-02 12:27:53 +0530

    3rd addfunction

--- myaddfunction.java
+++ myaddfunction.java
@@ -1,6 +1,14 @@
 this is my add function
+<<<<<<< Updated upstream
 7th day java line of code 
 8th day java line of code 
 9th day java line of code 
 10th day java line of code
+=======
+1st day java line of code
+2nd day java line of code
+3rd day java line of code
+4th day java line of code
+5th day java line of code
+6th day java line of code
 
